4fb80b90a3f90e4f56020d9e104e0079a29aa5c7
Lbniese <lbniese@gmail.com>
2020-08-28 10:19:21 +0200

    version 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] jokes = {"My new thesaurus is terrible. In fact, it's so bad, I'd say it's terrible.", "How are false teeth like stars? They come out at night!", "Why are skeletons so calm? Because nothing gets under their skin.", "I got fired from a florist, apparently I took too many leaves.", "Q: What’s 50 Cent’s name in Zimbabwe? A: 200 Dollars."};
+
+        Random r = new Random();
+        int randomNumber=r.nextInt(jokes.length);
+
+        System.out.println(jokes[randomNumber]);
+
+    }
+}
